5fdc02290aebff7bba93f7cf808de289b840acf0
uuamenator <uuamenator@gmail.com>
2021-07-20 13:47:27 +0300

    Update Tile.cs

--- Assets/Scenes/Tile.cs
+++ Assets/Scenes/Tile.cs
@@ -18,7 +18,7 @@ public abstract class Tile : MonoBehaviour
     public int index;
     public static int tilesCreated = 0;
 
-
+    //LOOOOL 4Head
 
     public string TileName { get => tileName; set => tileName = value; }
     public bool IsOccupied { get => isOccupied; set => isOccupied = value; }
